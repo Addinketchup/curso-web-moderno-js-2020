dc36b68686c20a63e7c7d64a6592089639676166
malqq <lenonalcant@hotmail.com>
2019-12-12 23:20:19 -0300

    Criado o diretorio fundamentos

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,16 @@
+//organizacao basica de um codigo em js
+
+//sentenca
+console.log("Sentença de Código")
+
+// bloco de código, delimitado por {}
+// funcao: agrupar sentencas de codigo
+
+//bloco 1
+{
+    //bloco 2
+    {
+        console.log("Ola!") //sentenca 1
+        console.log("Mundo!") //sentenca 2
+    }    
+}
