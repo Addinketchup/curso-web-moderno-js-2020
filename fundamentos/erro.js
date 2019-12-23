562f80fd6d52c8eb50bc645f1b3f891a116a2b49
devlam <lenonalcant@hotmail.com>
2019-12-23 17:19:46 -0300

    Add arquivos erro.js

---
+++ fundamentos/erro.js
@@ -0,0 +1,26 @@
+function tratarErroELancar(erro) {
+    //throw new Error('...')
+    //throw 10
+    //throw true
+    //throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+    
+}
+
+//const obj = { name: 'Roberto' } //Parametro "name" correto, nao da erro
+const obj = { nome: 'Roberto' } //Parametro "nome" errado
+imprimirNomeGritado(obj)
