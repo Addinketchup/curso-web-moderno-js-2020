bd1915545a770b4eace83ee1a03b4d9716ea0641
devlam <lenonalcant@hotmail.com>
2019-12-25 14:28:11 -0300

    Modificado arquivos cidadaoPrimeiraClasse.js, paramsERetornoSaoOpcionais.js

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -23,7 +23,7 @@ function run(fun) {
 
 run(function () { console.log('Executando...') })
 
-// Uma função pode retornar/conter uma outra função
+// Uma função pode retornar/conter uma outra função 
 function soma(a, b) {
     return function (c) {
         console.log(a + b + c)
