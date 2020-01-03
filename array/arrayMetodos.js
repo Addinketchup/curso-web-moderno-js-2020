ec7dceb95bcdc1108b7b5eaa3da5ec2e907bb8aa
malqq <lenonalcant@hotmail.com>
2020-01-03 20:02:41 -0300

    Add arquivos arrayMetodos.js, forEach1.js, simulandoArray.js

---
+++ array/arrayMetodos.js
@@ -0,0 +1,27 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o ultimo elemento do array
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento do array
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') // adiciona um elemento na primeira posicao
+console.log(pilotos)
+
+// splice pode adicinar e remover elementos:
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunsPilotos = pilotos.slice(2) // retorna um novo array
+console.log(algunsPilotos)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
