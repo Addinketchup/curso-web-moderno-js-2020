6d06b836c7a1c4ad535bd2c9ed9906cb3229b5d5
malqq <lenonalcant@hotmail.com>
2020-01-04 11:16:51 -0300

    Add arquivos map1.js, map2.js

---
+++ array/map2.js
@@ -0,0 +1,8 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os precos
