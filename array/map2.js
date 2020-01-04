f2dd23418dfbe811a4e8a16d5dcd8b25f2e102a1
malqq <lenonalcant@hotmail.com>
2020-01-04 15:34:01 -0300

    Solucao desafio map

--- array/map2.js
+++ array/map2.js
@@ -6,3 +6,17 @@ const carrinho = [
 ]
 
 // Retornar um array apenas com os precos
+// Minha solucao:
+const resultado = carrinho.map(function(e) {
+    let obj = JSON.parse(e)
+    return obj.preco
+})
+console.log(resultado)
+
+
+// Solucao professor:
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const res = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(res)
