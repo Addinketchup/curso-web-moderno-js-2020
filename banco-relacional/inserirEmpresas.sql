f418ff8ba350cc041ba2b50d7a9e68fba5ad7da9
lnnalcant <lenonalcant@hotmail.com>
2020-04-08 12:04:07 -0300

    Finalizada seção banco relacional

---
+++ banco-relacional/inserirEmpresas.sql
@@ -0,0 +1,8 @@
+INSERT INTO empresas
+    (nome, cnpj)
+VALUES
+    ('Bradesco', 95694186000132),
+    ('Vale', 27887148000146),
+    ('Cielo', 01598317000134);
+
+ALTER TABLE empresas MODIFY cnpj VARCHAR(14);
